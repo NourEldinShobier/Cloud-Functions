66688f05f0fe9de02791c4eb93367a873e344871
Nour Shobier <nour.eldin.shobier@gmail.com>
2020-10-12 13:59:15 +0200

    Storage and Callable

--- fireship-cloud-fn/functions/src/auth.ts
+++ fireship-cloud-fn/functions/src/auth.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 
-const db = admin.firestore();
+const db = admin.firestore(); // It's a performance practice to create DB out of functions
 
 export const createUserRecord = functions.auth
   .user()
--- fireship-cloud-fn/functions/src/index.ts
+++ fireship-cloud-fn/functions/src/index.ts
@@ -1,5 +1,7 @@
 export { basicHTTP, api } from './http';
 export { createUserRecord } from './auth';
+export { gameCount, userTrend } from './firestore';
+export { resizeAvatar } from './storage';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
